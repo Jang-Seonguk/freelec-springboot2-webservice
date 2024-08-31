879c3b4c54222efe75fa024bd9abbddf86660bd3
sunguk7158 <sunguk7158@naver.com>
2024-08-31 09:33:36 +0900

    commit

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 public interface PostsRepository extends JpaRepository<Posts, Long>{
 
     //@Query("SELECT p FROM Posts p ORDER BY p.id ASC")
-    @Query("SELECT p FROM Posts p")
+    @Query("SELECT p FROM posts p")
     List<Posts> findAllAsc();
 }
